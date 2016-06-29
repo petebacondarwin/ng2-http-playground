5822f7c23dc2d4b66338ce7ca783fc9d50fe7752
Peter Bacon Darwin <pete@bacondarwin.com>
2016-06-29 22:13:54 +0100

    add basic interactivity to the example

--- example/app.js
+++ example/app.js
@@ -1,9 +1,18 @@
+/*global angular*/
 angular.module('app', ['ng2-http'])
 
-.run(['ng2Http', function(ng2Http) {
+.run(['$rootScope', 'ng2Http', function($rootScope, ng2Http) {
   ng2Http.get('test.json')
     .map(response => response.json())
     .subscribe((data)=> {
       console.log(data);
     });
+
+  $rootScope.$watch('query.address', function(val) {
+    ng2Http.get('http://maps.googleapis.com/maps/api/geocode/json', { search: 'address=' + val })
+      .map(response => response.json().results.map(result => result.formatted_address))
+      .subscribe((data)=> {
+        console.log(data);
+      });
+  });
 }]);
